7b3ddf4715088b859c84ad69b49aad2fc91f4675
tatianahernandez <ltatianahernandez@sanmateo.edu.co>
2023-05-03 04:54:45 -0500

    cipher cesar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,48 @@
+const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 const cipher = {
-  // ...
-};
+  encode: function cifrar(offset, mensaje) {
+    let resultado = "";
+    //toUpperCase  convierte texto a mayusculas 
+    mensaje = mensaje.toUpperCase()
+
+    for (let i = 0; i < mensaje.length; i++) {
+      //reconocer cada una de las letras de mi mensaje
+      const letraMensaje = mensaje[i];
+      //traer indice de la letra de mi mensaje
+      const indice = letras.indexOf(letraMensaje)
+      // si la letra no existe devuelve -1
+      if (indice !== -1) {
+        const cifrado = (indice + offset) % 26
+        resultado += letras[cifrado]
+      } else if (letraMensaje === " ") {
+        resultado += " "
+      }
+    }
+
+    return resultado
+  },
+  decode: function descifrar(offset, mensaje) {
+    let resultado = "";
+    //toUpperCase  convierte texto a mayusculas 
+    mensaje = mensaje.toUpperCase()
+
+    for (let i = 0; i < mensaje.length; i++) {
+      //reconocer cada una de las letras de mi mensaje
+      const letraMensaje = mensaje[i];
+      //traer indice de la letra de mi mensaje
+      const indice = letras.indexOf(letraMensaje)
+      // si la letra no existe devuelve -1
+      if (indice !== -1) {
+        const cifrado = (26 + ((indice - offset)%26)) % 26
+        resultado += letras[cifrado]
+      } else if (letraMensaje === " ") {
+        resultado += " "
+      }
+    }
+
+    return resultado
+  }
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const iniciar = document.getElementById("inicio");
+iniciar.addEventListener("click", function () {
+  document.getElementById("principal").style.display = "none";
+  document.getElementById("secundaria").style.display = "block";
+});
+
+//declaro variable donde voy a guardar el mensaje
+let ms
+let mensajeCifrado
+const posicion = 33;
+const desmensaje = document.getElementById("resmensaje1");
+const btnCifrado = document.getElementById("btn2");
+//caja de texto donde voy a ingresar mi mensaje
+const mensaje = document.getElementById("mensaje");
+
+//agrega un evento que busca dentro del mensaje el input y con el evento e trae la terjeta con el valor
+mensaje.addEventListener("input", function (e) {
+  ms = e.target.value
+})
+//traer el elemento donde voy a mostrar el cifrado
+const elemento = document.getElementById("cifrado");
+
+btnCifrado.addEventListener("click", function () {
+  mensajeCifrado = cipher.encode(posicion,ms)
+  desmensaje.value = mensajeCifrado
+  elemento.textContent = mensajeCifrado;
+  console.log(mensajeCifrado)
+});
+
+const desCifrado = document.getElementById("btn3");
+//traer el elemento donde voy a mostrar el cifrado
+const deselemento = document.getElementById("descifrado");
+
+desCifrado.addEventListener("click", function () {
+  const mensajedesCifrado = cipher.decode(posicion,mensajeCifrado)
+  deselemento.textContent = mensajedesCifrado;
+});
